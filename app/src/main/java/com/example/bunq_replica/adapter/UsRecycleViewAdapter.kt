448ec0813d511ccecb1a88ad959ff0a91bd3a284
YusufCanerGüreş <gurescaner@gmail.com>
2020-09-14 15:01:30 +0300

    Us Page cardviews updated.

--- app/src/main/java/com/example/bunq_replica/adapter/UsRecycleViewAdapter.kt
+++ app/src/main/java/com/example/bunq_replica/adapter/UsRecycleViewAdapter.kt
@@ -12,6 +12,7 @@ import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.bunq_replica.R
 import com.example.bunq_replica.model.UsNewsModel
 
@@ -56,9 +57,10 @@ class UsRecycleViewAdapter(private val features: List<UsNewsModel>) :
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentItem = features[position]
-        holder.parentLayout.setBackgroundColor(Color.parseColor(currentItem.newsBackgroundColor))
+        holder.parentLayout.setCardBackgroundColor(Color.parseColor(currentItem.newsBackgroundColor))
         holder.txtRw.text = currentItem.newsText
         holder.tvTitle.text = currentItem.newsTitle
         holder.tvType.text = currentItem.newsType
@@ -68,14 +70,16 @@ class UsRecycleViewAdapter(private val features: List<UsNewsModel>) :
         holder.vTvShare.setTextColor(Color.parseColor(currentItem.newsColor))
 
 
-
         Glide.with(holder.itemView.context)
             .load(currentItem.newsUrl)
             .fitCenter()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(holder.imageUrl)
+
     }
 
     override fun getItemCount(): Int {
         return features.size
     }
+
 }
